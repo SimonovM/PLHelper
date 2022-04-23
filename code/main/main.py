52776defc8cbfffcf95f4834600d6140583532a8
SimonovM <simonovm1123@gmail.com>
2022-04-24 00:25:44 +0500

    Added Java, Python screens Refactor C# screen

--- code/main/main.py
+++ code/main/main.py
@@ -4,10 +4,11 @@ from kivy.uix.screenmanager import ScreenManager, FadeTransition
 
 from code.screens.c_sharp_lang_screen import CSharpLangScreen
 from code.screens.func_langs_choose_screen import FuncLangsChooseScreen
+from code.screens.java_lang_screen import JavaLangScreen
 from code.screens.lang_type_screen import LangTypeScreen
 from code.screens.main_screen import MainScreen
 from code.screens.imperative_langs_choose_screen import ImperativeLangsChooseScreen
-
+from code.screens.python_lang_screen import PythonLangScreen
 
 Builder.load_file("../interfaces/ui/rounded_button.kv")
 Builder.load_file("../interfaces/main_screen.kv")
@@ -15,6 +16,8 @@ Builder.load_file("../interfaces/lang_type_screen.kv")
 Builder.load_file("../interfaces/imperative_langs/imperative_langs_choose_screen.kv")
 Builder.load_file("../interfaces/func_langs/func_langs_choose_screen.kv")
 Builder.load_file("../interfaces/imperative_langs/C#.kv")
+Builder.load_file("../interfaces/imperative_langs/Java.kv")
+Builder.load_file("../interfaces/imperative_langs/Python.kv")
 
 
 class PLHelper(App):
@@ -30,6 +33,8 @@ class PLHelper(App):
         manager.add_widget(ImperativeLangsChooseScreen(name="imperative"))
         manager.add_widget(FuncLangsChooseScreen(name="func"))
         manager.add_widget(CSharpLangScreen(name="c#"))
+        manager.add_widget(JavaLangScreen(name="java"))
+        manager.add_widget(PythonLangScreen(name="python"))
         return manager
 
 
