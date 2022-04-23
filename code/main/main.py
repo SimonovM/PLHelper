df66bfc96401b064bb2e04c196d1246867330647
SimonovM <simonovm1123@gmail.com>
2022-04-23 17:50:11 +0500

    Added first screen and buttons

--- code/main/main.py
+++ code/main/main.py
@@ -1,14 +1,21 @@
 from kivy.app import App
-from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager
 
-Window.size = (800, 600)
-Window.title = "Справка по языкам программирования"
+from code.screens.main_screen import MainScreen
 
 
 class PLHelper(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.size = (800, 600)
+        self.title = "Справка по языкам программирования"
 
     def build(self):
-        return
+        manager = ScreenManager()
+        manager.add_widget(MainScreen(name="MainScreen"))
+        return Builder.load_file("../interfaces/main_interface.kv")
+
+
+if __name__ == "__main__":
+    PLHelper().run()
