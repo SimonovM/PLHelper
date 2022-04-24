ea196f77ebdd53337457ea1058e8fac8f194feb0
SimonovM <simonovm1123@gmail.com>
2022-04-24 10:43:14 +0500

    Added Erlang, Go, Scala, C++

--- code/main/main.py
+++ code/main/main.py
@@ -3,6 +3,7 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, FadeTransition
 
 from code.screens.c_sharp_lang_screen import CSharpLangScreen
+from code.screens.erlang_lang_screen import ErlangLangScreen
 from code.screens.f_sharp_lang_screen import FSharpLangScreen
 from code.screens.func_langs_choose_screen import FuncLangsChooseScreen
 from code.screens.haskell_lang_screen import HaskellLangScreen
@@ -12,6 +13,9 @@ from code.screens.lisp_lang_screen import LispLangScreen
 from code.screens.main_screen import MainScreen
 from code.screens.imperative_langs_choose_screen import ImperativeLangsChooseScreen
 from code.screens.python_lang_screen import PythonLangScreen
+from code.screens.cplus_lang_screen import CPlusLangScreen
+from code.screens.go_lang_screen import GoLangScreen
+from code.screens.scala_lang_screen import ScalaLangScreen
 
 Builder.load_file("../interfaces/ui/rounded_button.kv")
 Builder.load_file("../interfaces/main_screen.kv")
@@ -24,7 +28,10 @@ Builder.load_file("../interfaces/imperative_langs/Python.kv")
 Builder.load_file("../interfaces/func_langs/F#.kv")
 Builder.load_file("../interfaces/func_langs/Haskell.kv")
 Builder.load_file("../interfaces/func_langs/Lisp.kv")
-
+Builder.load_file("../interfaces/imperative_langs/C++.kv")
+Builder.load_file("../interfaces/imperative_langs/Go.kv")
+Builder.load_file("../interfaces/func_langs/Erlang.kv")
+Builder.load_file("../interfaces/func_langs/Scala.kv")
 
 class PLHelper(App):
     def __init__(self, **kwargs):
@@ -44,6 +51,10 @@ class PLHelper(App):
         manager.add_widget(FSharpLangScreen(name="f#"))
         manager.add_widget(HaskellLangScreen(name="haskell"))
         manager.add_widget(LispLangScreen(name="lisp"))
+        manager.add_widget(CPlusLangScreen(name="cplus"))
+        manager.add_widget(GoLangScreen(name="go"))
+        manager.add_widget(ErlangLangScreen(name="erlang"))
+        manager.add_widget(ScalaLangScreen(name="scala"))
         return manager
 
 
